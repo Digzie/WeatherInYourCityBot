c4279f658752e8f8d2ecf9cd620956bdf47d1090
Digzie <brawlstarstopgame2@gmail.com>
2025-05-06 19:36:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,6 @@ logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     handlers=[
         logging.FileHandler('app.log'), 
-        logging.StreamHandler() 
     ]
 )
 
